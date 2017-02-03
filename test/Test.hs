96cfba7f639a2644d185eb10bd6d84d4020a6cc1
Alexander Thiemann <mail@athiemann.net>
2017-02-03 12:38:03 +0100

    remove hspec discover

--- test/Test.hs
+++ test/Test.hs
@@ -1,1 +1,10 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module  Main where
+
+import qualified Data.HVectSpec
+
+import Test.Hspec
+
+main :: IO ()
+main =
+    hspec $
+    Data.HVectSpec.spec
